47ab83c7bde20a1208543cc2829bd2b7848bd5db
ucevstudent <ethirajshanmugam94@gmail.com>
2026-07-17 09:36:25 +0530

    Add files via upload

---
+++ basic_combinational/fulladder/fulladder_tb.v
@@ -0,0 +1,21 @@
+module full_adder_tb;
+reg a,b,c;
+wire sum,carry;
+full_adder fa1(
+.a(a),.b(b),.c(c),.sum(sum),.carry(carry));
+initial begin
+$monitor("time=%0t a=%b,c=%b,sum=%b,carry=%b",$time,a,b,c,sum,carry);
+end
+initial begin
+a=0;b=0;c=0;#10;
+a=0;b=0;c=1;#10;
+a=0;b=1;c=0;#10;
+a=0;b=1;c=1;#10;
+a=1;b=0;c=0;#10;
+a=1;b=0;c=1;#10;
+a=1;b=1;c=0;#10;
+a=1;b=1;c=1;#10;
+$finish;
+end
+endmodule
+
